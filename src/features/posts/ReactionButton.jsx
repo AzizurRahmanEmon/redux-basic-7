d87a934fbf70c8930b22400c1bcb74ec23263755
Azizur Rahman <azizurrahmanemon007@gmail.com>
2023-11-11 01:43:07 +0600

    final

--- src/features/posts/ReactionButton.jsx
+++ src/features/posts/ReactionButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import { reactionAdded } from './postsSlice'
+import { useAddReactionMutation } from './postsSlice'
+
 const reactionEmoji = {
     thumbsUp: '👍',
     wow: '😮',
@@ -10,15 +10,17 @@ const reactionEmoji = {
 }
 
 const ReactionButton = ({post}) => {
-    const dispatch = useDispatch()
-
+    const [addReaction] = useAddReactionMutation()
     const reactionButtons = Object.entries(reactionEmoji).map(([name,emoji],index) => {
         return (
             <button
             key={index}
             type='button'
             className='reactionButton'
-            onClick={() => dispatch(reactionAdded({postId: post.id, reaction: name}))}
+            onClick={() => {
+                const newValue = post.reactions[name] + 1;
+                addReaction({postId: post.id, reactions: {...post.reactions, [name]: newValue}})
+            }}
             >
                 {emoji}{post.reactions[name]}
             </button>
